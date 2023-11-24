a91a3a2b9805e41d3059e4fcb2c1dd4e60fbdaca
my-alaska <piotrztych@wp.pl>
2023-11-24 19:59:49 +0100

    knowledge graphs init

--- bandits/bandit_framework.py
+++ bandits/bandit_framework.py
@@ -1,27 +1,43 @@
-import matplotlib.pyplot as plt
 from random import random
-from typing import Dict, List, Callable
+from typing import Callable, Dict, List
+
+import matplotlib.pyplot as plt
 
 
 class Bandit:
     def __init__(self, bandit_id: str, arm_ids: List[str]):
-        self.bandit_id = bandit_id # name of the bandit to be displayed
-        self.arm_ids = arm_ids # list od IDs of all the existing arms
-    
-    def recommend(self, size: int) -> List[str]: # method should return list of the size `size` containing recommended items from arm_ids
+        self.bandit_id = bandit_id  # name of the bandit to be displayed
+        self.arm_ids = arm_ids  # list od IDs of all the existing arms
+
+    def recommend(
+        self, size: int
+    ) -> List[
+        str
+    ]:  # method should return list of the size `size` containing recommended items from arm_ids
         raise NotImplementedError()
-    
-    def feedback(self, arm_id: str, payout: float): # helper method - bandit gets results of its recommendation and can store the results
+
+    def feedback(
+        self, arm_id: str, payout: float
+    ):  # helper method - bandit gets results of its recommendation and can store the results
         raise NotImplementedError()
 
 
 class Arm:
-    def __init__(self, arm_id: str, activation_probability: float, payout_function: Callable[[], float]):
+    def __init__(
+        self,
+        arm_id: str,
+        activation_probability: float,
+        payout_function: Callable[[], float],
+    ):
         self.arm_id = arm_id
-        self.activation_probability = activation_probability # how likely is that the arm would yield any payout
-        self.payout_function = payout_function # function called to calculate payout
+        self.activation_probability = (
+            activation_probability  # how likely is that the arm would yield any payout
+        )
+        self.payout_function = payout_function  # function called to calculate payout
 
-    def pull(self): # returns the value of the ayout function or 0, according to the activation probability
+    def pull(
+        self,
+    ):  # returns the value of the ayout function or 0, according to the activation probability
         if random() <= self.activation_probability:
             return max(self.payout_function(), 0)
         else:
@@ -32,10 +48,12 @@ class Runner:
     def __init__(self, arms: Dict[str, Arm], bandits: List[Bandit]):
         self.arms = arms
         self.bandits = bandits
-    
+
     # this method runs each and every bandit algorithm `runs` times, for given `epochs` in each run
     #   and returns all the results and payouts which then can be plotted by the latter method
-    def simulate(self, runs: int, epochs: int, recommendation_size: int) -> Dict[str, List[List[float]]]:
+    def simulate(
+        self, runs: int, epochs: int, recommendation_size: int
+    ) -> Dict[str, List[List[float]]]:
         results = {}
         for bandit in self.bandits:
             print("Simulating: %s" % bandit.bandit_id)
@@ -52,8 +70,15 @@ class Runner:
                     run_results.append(epoch_payout)
                 results[bandit.bandit_id].append(run_results)
         return results
-    
-    def plot_results(self, results: Dict[str, List[List[float]]], runs: int, epochs: int, mode='cumulative', scale='linear'):
+
+    def plot_results(
+        self,
+        results: Dict[str, List[List[float]]],
+        runs: int,
+        epochs: int,
+        mode="cumulative",
+        scale="linear",
+    ):
         average = {bandit_id: [] for bandit_id in results}
         cumulative = {bandit_id: [] for bandit_id in results}
         for bandit_id in results:
@@ -61,28 +86,30 @@ class Runner:
                 epoch_results = []
                 for r in range(runs):
                     epoch_results.append(results[bandit_id][r][e])
-                
+
                 avg_result = sum(epoch_results) / runs
                 average[bandit_id].append(avg_result)
                 if e == 0:
                     cumulative[bandit_id].append(avg_result)
                 else:
                     cumulative[bandit_id].append(avg_result + cumulative[bandit_id][-1])
-        
-        if mode == 'cumulative':
+
+        if mode == "cumulative":
             self.print_aggregated_results(cumulative)
             for bandit_id in cumulative:
                 plt.plot(cumulative[bandit_id], label=bandit_id)
-        elif mode == 'average':
+        elif mode == "average":
             self.print_aggregated_results(average)
             for bandit_id in average:
                 plt.plot(average[bandit_id], label=bandit_id)
-        
+
         plt.yscale(scale)
         plt.legend()
         plt.show()
 
     def print_aggregated_results(self, aggregated: Dict[str, float]):
         print("\nAggregated results:\n")
-        for bandit_id, total_payout in sorted(aggregated.items(), key=lambda x: x[1][-1], reverse=True):
-            print('%s: %s' % (bandit_id, total_payout[-1]))
+        for bandit_id, total_payout in sorted(
+            aggregated.items(), key=lambda x: x[1][-1], reverse=True
+        ):
+            print("%s: %s" % (bandit_id, total_payout[-1]))
